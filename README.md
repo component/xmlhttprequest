87f5355c39a0ba1095b503d0a81a8062295486b7
Roman Shtylman <shtylman@gmail.com>
2014-01-21 14:16:24 -0500

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# xmlhttprequest
+
+Returns an XMLHttpRequest object or an ActiveXObject for fallback. This module provides api compatibility to the node.js module of the same name.
+
+## xmlhttprequest(opt)
+
+Options change the behavior during instantiation of the new xml request instance.
+
+* xdomain
+> The request will be cross domain so only return a working request instance if cross domain is supported
---
+++ index.js
@@ -0,0 +1,18 @@
+var hasCORS = require('has-cors');
+
+module.exports = function(opts) {
+  var xdomain = opts.xdomain;
+
+  // XMLHttpRequest can be disabled on IE
+  try {
+    if ('undefined' != typeof XMLHttpRequest && (!xdomain || hasCORS)) {
+      return new XMLHttpRequest();
+    }
+  } catch (e) { }
+
+  if (!xdomain) {
+    try {
+      return new ActiveXObject('Microsoft.XMLHTTP');
+    } catch(e) { }
+  }
+}
